c54feec36d111b18756d99883ccda96f7937bf47
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-08-27 14:10:44 +0530

    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,9 @@
 import React from "react";
+import { guide } from "../assets";
 
 const Button = ({ styles }) => {
   return (
-    <a
-      href="https://docs.google.com/forms/d/e/1FAIpQLSeuLZRKyxBO0r-NgVWiTGmr8vb7uUu5Ecr-wAaLihXo5zE_gQ/viewform"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a href={guide} target="_blank" rel="noopener noreferrer">
       <button
         type="button"
         className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
