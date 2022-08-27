dbcfffd9d8e73f4cfebe4083fabc5873b86841fd
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-08-27 14:12:12 +0530

    TEXT

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ styles }) => {
         type="button"
         className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
       >
-        Register For Round 1
+        Download Guidelines For Round 1
       </button>
     </a>
   );
